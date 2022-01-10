6923801401545ca1e27371d69ce1c81bee86882b
xebec19 <rohant946@gmail.com>
2022-01-10 22:28:55 +0530

    save

--- src/utils/errorHandler.middleware.ts
+++ src/utils/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
+/* eslint-disable import/prefer-default-export */
 export const errorHandler = (fn:(req:Request, res:Response) => void) => async (req:Request, res:Response,next:NextFunction) => {
         try{
             await fn(req, res);
--- src/utils/setResponse.method.ts
+++ src/utils/setResponse.method.ts
@@ -1,7 +1,8 @@
-import { ResponseMethod } from "../abstractions/classes/response.class";
-import { IResponse } from "../abstractions/interfaces/response.model";
+import { ResponseMethod } from '../abstractions/classes/response.class';
+import { IResponse } from '../abstractions/interfaces/response.model';
 
+ /* eslint-disable import/prefer-default-export */
 export const setResponse = (data:any,status:boolean,message:string):IResponse => {
     const setPayload = new ResponseMethod(status,message,data);
     return setPayload.payload;
-}
+};
