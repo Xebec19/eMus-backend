39998fe7aa8d2feb14cddc837932f9c7409c490c
xebec19 <rohant946@gmail.com>
2022-02-04 22:22:49 +0530

    trying to use script to commit

---
+++ src/controllers/public.controllers.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const home = async (req:Request,res:Response) => {
+    res.render('home');
+};
+
+export default home;
---
+++ src/routes/public.route.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import home from '../controllers/public.controllers';
+import { errorHandler } from '../utils/errorHandler.middleware';
+
+const router = express.Router();
+
+router.get('/home',errorHandler(home))
