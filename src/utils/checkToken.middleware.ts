223ce93fdb05771ebad99ee93772322a88bb8986
xebec19 <rohant946@gmail.com>
2022-06-03 23:22:44 +0530

    lint

--- src/utils/checkToken.middleware.ts
+++ src/utils/checkToken.middleware.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from "express";
-import { IResponse } from "../abstractions/interfaces/index.model";
-import { jwtCheck } from "./jwt.utils";
+import { NextFunction, Request, Response } from 'express';
+import { IResponse } from '../abstractions/interfaces/index.model';
+import { jwtCheck } from './jwt.utils';
 
 export const checkToken = (req:Request, res:Response, next:NextFunction) => {
     let response: IResponse;
     jwtCheck(''); // todo pass token in header as param and then check in user
-}
+};
