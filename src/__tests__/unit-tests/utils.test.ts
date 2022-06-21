47a459b3c3b7f5aaec22eb7c178b2732b415d985
xebec19 <rohant946@gmail.com>
2022-06-21 20:03:19 +0530

    wip add members

--- src/__tests__/unit-tests/utils.test.ts
+++ src/__tests__/unit-tests/utils.test.ts
@@ -1,9 +1,10 @@
-import { randomString } from '../../utils/index';
+import { Logger, randomString } from '../../utils/index';
 
 describe("Test utils functions", () => {
     
     test("Function for random string should be generate random string of length 10 by default", async () => {
         const resultString = await randomString();
+        Logger.info({resultString}); // todo remove log
         expect(resultString.length).toBe(10);
     });
 })
---
+++ wiki/db_logs/21-06-2022.sql
@@ -0,0 +1,2 @@
+alter table permissions drop column created_by;
+alter table permissions drop column updated_by;
